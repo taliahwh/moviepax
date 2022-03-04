aff0f93185cad97575b7941442d488539cd7cd83
Taliah Wharton <taliahwh23@yahoo.com>
2022-03-04 11:45:23 -0500

    dropdown implementation

--- client/src/components/TrendingSection.js
+++ client/src/components/TrendingSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
 import { LeftArrow, RightArrow } from './Arrows';
@@ -31,15 +31,20 @@ const TrendingSection = () => {
   return (
     <div className="flex flex-col space-y-2 h-[28rem] mx-auto max-w-8xl px-8 pt-12  sm:py-8 sm:px-10 md:px-20 ">
       {/* Header */}
-      <div className="flex space-x-5 items-center pb-4 ">
+      <div className="flex space-x-5 items-center pb-4">
         <h3 className="text-2xl font-semibold text-secondary">Trending</h3>
-        <div className="flex space-x-3 border-1 border-secondary rounded-full">
+
+        <div className="space-x-3 border-1 border-secondary rounded-full hidden md:flex">
           <p className="active bg-secondary px-5 py-1 rounded-full font-semibold text-white">
             Today
           </p>
           <p className="px-5 py-1 font-semibold">This Week</p>
         </div>
+        <div className="md:hidden">
+          <Dropdown />
+        </div>
       </div>
+
       {/* Content */}
       {/* <div className="grid grid-cols-6 gap-5"> */}
       <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
@@ -59,7 +64,7 @@ const TrendingSection = () => {
   );
 };
 
-function Card({ onClick }) {
+const Card = ({ onClick }) => {
   const visibility = React.useContext(VisibilityContext);
 
   return (
@@ -72,6 +77,59 @@ function Card({ onClick }) {
       <MovieCard />
     </div>
   );
-}
+};
+
+const Dropdown = () => {
+  return (
+    <>
+      <button
+        id="dropdownButton"
+        data-dropdown-toggle="dropdown"
+        className="text-white font-semibold bg-secondary text-md focus:ring-3 focus:ring-blue-300 rounded-full px-5 py-1 text-center inline-flex items-center"
+        type="button"
+      >
+        When{' '}
+        <svg
+          className="ml-2 w-4 h-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 9l-7 7-7-7"
+          ></path>
+        </svg>
+      </button>
+
+      <div
+        id="dropdown"
+        className="hidden z-10 w-28 text-base list-none bg-white rounded divide-y divide-gray-100 border-1 border-secondary"
+      >
+        <ul className="py-1" aria-labelledby="dropdownButton">
+          <li>
+            <a
+              href="/"
+              className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+            >
+              Today
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+            >
+              This Week
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default TrendingSection;
