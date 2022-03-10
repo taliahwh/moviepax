11fafd74ecf3c3ad2ecce447f710189811acbf52
Taliah Wharton <taliahwh23@yahoo.com>
2022-03-10 16:00:28 -0500

    tvDetails section added

--- client/src/screens/DetailsScreen.js
+++ client/src/screens/DetailsScreen.js
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 import OverviewHeader from '../components/OverviewHeader';
 import SeriesCastSection from '../components/SeriesCastSection';
 import Footer from '../components/Footer';
 
-import { getMovieDetails } from '../actions/mediaActions';
+import {
+  TV_DETAILS_FAILURE,
+  MOVIE_DETAILS_FAILURE,
+} from '../constants/mediaContstants';
+import { getMovieDetails, getTVDetails } from '../actions/mediaActions';
 
 const DetailsScreen = () => {
+  const location = useLocation();
   const dispatch = useDispatch();
   const { id } = useParams();
+
+  // MOVIE STATE
 
   const {
     loading: loadingMovieDetails,
@@ -33,9 +40,38 @@ const DetailsScreen = () => {
     movieKeywords,
   } = useSelector((state) => state.movieKeywords);
 
+  // TV STATE
+
+  const {
+    loading: loadingTVDetails,
+    error: errorTVDetails,
+    success: successTVDetails,
+    tvDetails,
+  } = useSelector((state) => state.tvDetails);
+
+  const {
+    loading: loadingTVCast,
+    error: errorTVCast,
+    success: successTVCast,
+    tvCast,
+  } = useSelector((state) => state.tvCast);
+
+  const {
+    loading: loadingTVKeywords,
+    error: errorTVKeywords,
+    success: successTVKeywords,
+    tvKeywords,
+  } = useSelector((state) => state.tvKeywords);
+
   useEffect(() => {
-    dispatch(getMovieDetails(id));
-  }, [dispatch, id]);
+    if (location.pathname.includes('/movie')) {
+      dispatch({ type: TV_DETAILS_FAILURE });
+      dispatch(getMovieDetails(id));
+    } else if (location.pathname.includes('/tv')) {
+      dispatch({ type: MOVIE_DETAILS_FAILURE });
+      dispatch(getTVDetails(id));
+    }
+  }, [dispatch, id, location]);
   return (
     <div>
       {successMovieDetails && successMovieCast && successMovieKeywords && (
@@ -46,6 +82,19 @@ const DetailsScreen = () => {
             cast={movieCast.data.cast}
             details={movieDetails.data}
             keywords={movieKeywords.data.keywords}
+          />
+          <Footer />
+        </>
+      )}
+
+      {successTVDetails && successTVCast && successTVKeywords && (
+        <>
+          <OverviewHeader item={tvDetails.data} />
+          <SeriesCastSection
+            item={tvDetails.data}
+            cast={tvCast.data.cast}
+            details={tvDetails.data}
+            keywords={tvKeywords.data.results}
           />
           <Footer />
         </>
