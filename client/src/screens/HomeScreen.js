cefbd5c77bdb02b6a046cdf3dc05825d9b9d5bf6
Taliah Wharton <taliahwh23@yahoo.com>
2022-03-10 13:01:29 -0500

    removed freeToWatchSection

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -14,7 +14,6 @@ const HomeScreen = () => {
       <TrendingSection />
       <WhatsPopularSection />
       <LatestTrailersSection />
-      <FreeToWatchSection />
       <Footer />
     </div>
   );
