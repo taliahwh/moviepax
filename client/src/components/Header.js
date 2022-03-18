81db3fa2ea6658f8b0b35b14c6d0ac11cc80c921
Taliah Wharton <taliahwh23@yahoo.com>
2022-03-18 17:13:19 -0400

    refactor header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 import movipax_logo from '../assets/movipax_logo.png';
 
