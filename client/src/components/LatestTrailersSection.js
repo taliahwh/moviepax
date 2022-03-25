044d8b60cdc0baac4ceab3f5496a879ec2809bf5
Taliah Wharton <taliahwh23@yahoo.com>
2022-03-25 11:42:36 -0400

    trailer implementation

--- client/src/components/LatestTrailersSection.js
+++ client/src/components/LatestTrailersSection.js
@@ -1434,7 +1434,7 @@ const Dropdown = () => {
           </li>
           <li>
             <Link
-              to="/"
+              to="/trailers/movies"
               className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
             >
               Movies
